c6ca1e35bbc5a7668564b6a4d76c9cf82da48225
José Augusto <joseaugusto.mello01@gmail.com>
2022-12-13 08:55:31 -0300

    Projeto Criado

---
+++ src/main/java/com/AugustoMello09/demo/DomainApplication.java
@@ -0,0 +1,13 @@
+package com.AugustoMello09.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AugustoMello09/demo/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AugustoMello09.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
